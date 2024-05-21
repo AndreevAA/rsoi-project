8761119d65c4f765db7c694bcefc1c20baf8331a
Andreev Alexander <andreev.business.mail@gmail.com>
2024-05-21 12:20:41 +0300

    update

--- helm_reinstall_all.sh
+++ helm_reinstall_all.sh
@@ -1,4 +1,7 @@
+kubectl delete all --all -n default
+kubectl delete ingress controller -n default
 helm uninstall services k8s/services-chart/
 helm uninstall postgres k8s/postgres-chart/
 helm install postgres k8s/postgres-chart/
 helm install services k8s/services-chart/
+kubectl apply -f k8s/load_balancer.yaml
