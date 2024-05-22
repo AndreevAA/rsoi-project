748b752f62ddc7f0b249733b052b3b46ce4da6e0
Andreev Alexander <andreev.business.mail@gmail.com>
2024-05-22 10:54:14 +0300

    update

--- gateway_service/gateway_service/auth.py
+++ gateway_service/gateway_service/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Depends
 from fastapi.security import OAuth2AuthorizationCodeBearer
 
-oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl='login', tokenUrl='token')
+oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl='/api/gateway-service/login', tokenUrl='/api/gateway-service/token')
 
 
 async def get_user_token(token: str = Depends(oauth2_scheme)) -> str:
