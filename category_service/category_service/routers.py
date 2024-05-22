9fa9f00ce5ab874a74a6e8b0f558a090f825db21
Andreev Alexander <andreev.business.mail@gmail.com>
2024-05-22 16:13:25 +0300

    update category routes

--- category_service/category_service/routers.py
+++ category_service/category_service/routers.py
@@ -10,7 +10,7 @@ from category_service.schemas import CategoryModel, CategoryPage, InputCategory,
 router = APIRouter()
 
 
-@router.get('/categories', status_code=status.HTTP_200_OK, response_model=CategoryPage)
+@router.get('/api/category-service/categories', status_code=status.HTTP_200_OK, response_model=CategoryPage)
 async def get_categories(
     namespace_id: UUID,
     page: int = 1,
@@ -33,7 +33,7 @@ async def get_categories(
     return CategoryPage(page=page, size=size, total_elements=result_count, items=categories)
 
 
-@router.get('/categories/{category_id}', status_code=status.HTTP_200_OK, response_model=CategoryModel)
+@router.get('/api/category-service/categories/{category_id}', status_code=status.HTTP_200_OK, response_model=CategoryModel)
 async def get_category(
     category_id: UUID,
     repo: CategoryRepository = Depends(get_category_repo),
@@ -42,7 +42,7 @@ async def get_category(
     return await repo.get_category(category_id)
 
 
-@router.post('/categories', status_code=status.HTTP_201_CREATED, response_model=CategoryModel)
+@router.post('/api/category-service/categories', status_code=status.HTTP_201_CREATED, response_model=CategoryModel)
 async def create_category(
     category: InputCategory,
     repo: CategoryRepository = Depends(get_category_repo),
@@ -54,7 +54,7 @@ async def create_category(
     return category
 
 
-@router.delete('/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/api/category-service/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_category(
     category_id: UUID,
     repo: CategoryRepository = Depends(get_category_repo),
@@ -66,7 +66,7 @@ async def delete_category(
     await kafka.pull(f'Deleted category {category.name} from namespace {category.namespace_id}')
 
 
-@router.get('/notes/{note_id}/categories', status_code=status.HTTP_200_OK, response_model=NoteCategories)
+@router.get('/api/category-service/notes/{note_id}/categories', status_code=status.HTTP_200_OK, response_model=NoteCategories)
 async def get_note_categories(
     note_id: UUID,
     repo: CategoryRepository = Depends(get_category_repo),
@@ -76,7 +76,7 @@ async def get_note_categories(
     return NoteCategories(items=categories)
 
 
-@router.post('/notes/{note_id}/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.post('/api/category-service/notes/{note_id}/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def add_category_to_note(
     note_id: UUID,
     category_id: UUID,
@@ -89,7 +89,7 @@ async def add_category_to_note(
     await kafka.pull(f'Added category {category.name} to note {note_id}')
 
 
-@router.delete('/notes/{note_id}/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/api/category-service/notes/{note_id}/categories/{category_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_category_from_note(
     note_id: UUID,
     category_id: UUID,
